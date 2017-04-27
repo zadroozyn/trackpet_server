8afe61ba308dd3536dc87685e46893b947246181
Michał Zadrużyński <michal@on-running.com>
2017-04-27 16:16:24 +0200

    add test response

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ var app = express();
 app.get('/', function(req, res){
   res.send('hello world');
 });
-
+app.post('/api/v1/pet_location', function(req, res){
+  	if(req.get('auth') == 'aUqVokPnFANRdAANLTMKTb8c') {
+		res.send('Pet: '+'Paulina);
+	} else {
+		res.sendStatus(403);
+	}
+});
 app.listen(5000);
